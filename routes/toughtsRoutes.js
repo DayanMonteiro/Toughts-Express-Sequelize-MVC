c03d72f92ecb9b51cafcf79c8204a6a01660626c
DayanMonteiro <dmsmamb@gmail.com>
2023-11-27 19:14:15 -0300

    thought creation form

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -6,6 +6,7 @@ const ToughtController = require("../controllers/ToughtController");
 const checkAuth = require("../helpers/auth").checkAuth;
 
 // passa a função na rota dessa forma o checkAuth só permite acesso quando estiver logado
+router.get("/add", checkAuth, ToughtController.createTought);
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
 router.get("/", ToughtController.showToughts);
 
