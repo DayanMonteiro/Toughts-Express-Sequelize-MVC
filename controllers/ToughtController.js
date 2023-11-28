ac33e44fa1a5880b6bf11c549965b808e0de5504
DayanMonteiro <dmsmamb@gmail.com>
2023-11-28 17:29:06 -0300

    dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,7 +7,27 @@ module.exports = class ToughtController {
   }
 
   static async dashboard(req, res) {
-    res.render("toughts/dashboard");
+    const userId = req.session.userid;
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      // when it catches the user it already brings all his thoughts
+      include: Tought,
+      plain: true,
+    });
+
+    // check if user exists
+    if (!user) {
+      res.redirect("login");
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues);
+
+    //  console.log("pensamentos do usuario", toughts);
+
+    res.render("toughts/dashboard", { toughts });
   }
 
   static createTought(req, res) {
