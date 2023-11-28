8ed288e536d90e40f12c5ceca81e7d8d2bfdc2aa
DayanMonteiro <dmsmamb@gmail.com>
2023-11-28 10:48:59 -0300

    insert tought in database

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,7 @@ const checkAuth = require("../helpers/auth").checkAuth;
 
 // passa a função na rota dessa forma o checkAuth só permite acesso quando estiver logado
 router.get("/add", checkAuth, ToughtController.createTought);
+router.post("/add", checkAuth, ToughtController.createToughtSave);
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
 router.get("/", ToughtController.showToughts);
 
